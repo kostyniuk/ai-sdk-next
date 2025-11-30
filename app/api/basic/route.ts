c087b3b6b78943884994d2773b9059f2815fcb72
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-11-30 21:01:33 +0100

    add schema

--- app/api/basic/route.ts
+++ app/api/basic/route.ts
@@ -1,10 +1,25 @@
 import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
+import { generateObject, generateText } from 'ai';
 import { NextResponse } from 'next/server';
+import { z } from 'zod';
+
+const schema = z.object({
+    name: z.string().describe('The name of the recipe'),
+    ingredients: z.array(
+        z.object({
+            name: z.string().describe('The name of the ingredient'),
+            quantity: z.number().describe('The quantity of the ingredient'),
+            unit: z.string().describe('The unit of the ingredient'),
+        })
+    ).describe('The ingredients of the recipe'),
+    instructions: z.array(
+        z.string().describe('The description of the step'),
+    ).describe('The instructions of the recipe'),
+}).describe('The recipe schema');
 
 export async function GET(request: Request) {
     
-    const { text } = await generateText({
+    const { object } = await generateObject({
         model: google('gemini-2.5-flash'),
         prompt: 'Write a vegetarian lasagna recipe for 4 people.' + 
         'Return only the recipe, no other text.' +
@@ -15,8 +30,9 @@ export async function GET(request: Request) {
         '3. Ingredient 3' +
         'Instructions: ' +
         '1. Step 1' +
-        '2. Step 2'
+        '2. Step 2',
+        schema,
     });
 
-  return NextResponse.json({ text });
+  return NextResponse.json({ object });
 }
